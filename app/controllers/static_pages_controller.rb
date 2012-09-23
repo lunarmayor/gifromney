857d2a77e24f8eab4057555f0c969da10d07c98a
Michael Morrissey <mikehmorrissey@gmail.com>
2012-09-23 13:46:00 -0500

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+def home
+
+	respond_to do |format|
+		format.html {render :html => @home}
+		format.js
+	end
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catboombox::Application.config.secret_token = 'be1c956d85626b89f040cc84225113e38d4470d83b10628f10f86afca37abe31c03275e546d15657dda39dfadab596619de9b7e2565321789456ccb03a43cedd'
